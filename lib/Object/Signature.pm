e5d2ad3b497beb74d18f4d8e0a62da33f1db2adc
Adam Kennedy <adamk@cpan.org>
2005-12-26 10:27:36 +0000

    Import of ADAMK/Object-Signature-1.03 from CPAN.
    gitpan-cpan-distribution: Object-Signature
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         ADAMK/Object-Signature-1.03.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/Object/Signature.pm
+++ lib/Object/Signature.pm
@@ -1,13 +1,15 @@
 package Object::Signature;
 
 use strict;
-use prefork 'Storable';
-use prefork 'Digest::MD5';
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.02';
+	$VERSION = '1.03';
 }
+
+# In the event prefork is loaded, apply prefork load-optimisation
+eval "use prefork 'Storable';";
+eval "use prefork 'Digest::MD5';";
 
 # The main (only) method.
 # The prototype only applies when called as a function.
@@ -40,12 +42,12 @@ Signature - Generate cryptographic signatures for objects
 
 =head1 DESCRIPTION
 
-Object::Signature is an abstract base class that you can inherit from in
+L<Object::Signature> is an abstract base class that you can inherit from in
 order to allow your objects to generate unique cryptographic signatures.
 
-The method used to generate the signature is based on Storable and
-Digest::MD5. The object is fed to Storable::nfreeze to get a string,
-which is then passed to Digest::MD5::md5_hex to get a unique 32
+The method used to generate the signature is based on L<Storable> and
+L<Digest::MD5>. The object is fed to C<Storable::nfreeze> to get a string,
+which is then passed to L<Digest::MD5::md5_hex> to get a unique 32
 character hexidecimal signature.
 
 =head1 METHODS
@@ -55,26 +57,41 @@ character hexidecimal signature.
 The C<signature> method is the only method added to your class, and will
 generate a unique 32 hexidecimal signature for any object it is called on.
 
-=head1 TO DO
-
-Allow Object::Signature to interact correctly with classes that are
-already Storable subclasses, supporting their own Storable hooks.
-
 =head1 SUPPORT
 
 All bugs should be filed via the bug tracker at
 
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Object%3A%3ASignature>
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Object-Signature>
 
 For other issues, or commercial enhancement or support, contact the author.
 
+=head1 TO DO
+
+=head2 Incremental Generation
+
+Currently has to generate the entire Storable string before digesting
+it. Would be nice if there was a way to incrementally Storablise and Digest
+in one pass so that it becomes much more memory efficient for large objects.
+
+=head2 Strengthen the Digest Algorithm
+
+Once the current (as of 2005) hashing controversy settles down, consider
+selecting a newer and more powerful hashing algorithm to replace MD5. Or
+offer alternatives depending on how important the security situation is,
+as MD5 is B<very> fast (90 meg a second) and many more secure ones are a
+lot slower (worse than 1/10th the speed in some cases).
+
+On our side is the fact we use Storable. It should be B<much> harder to create
+collisions when you don't control the string, only the structure B<before>
+it goes through Storable.
+
 =head1 AUTHORS
 
-Adam Kennedy (Maintainer), L<http://ali.as/>, cpan@ali.as
+Adam Kennedy E<lt>cpan@ali.asE<gt>, L<http://ali.as/>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2004 Adam Kennedy. All rights reserved.
+Copyright (c) 2004, 2005 Adam Kennedy. All rights reserved.
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
 
